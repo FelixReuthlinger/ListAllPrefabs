85004e782ec27304a51dcd69247eebdea6938803
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-05-14 16:41:16 +0200

    renaming from template

--- ListAllPrefabsPlugin.cs
+++ ListAllPrefabsPlugin.cs
@@ -4,21 +4,21 @@ using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 
-namespace ModTemplate;
+namespace ListAllPrefabs;
 
 [BepInPlugin(ModGuid, ModName, ModVersion)]
-public class ModTemplatePlugin : BaseUnityPlugin
+public class ListAllPrefabsPlugin : BaseUnityPlugin
 {
-    private const string ModName = "ModTemplate";
+    private const string ModName = "ListAllPrefabs";
     private const string ModVersion = "1.0.0";
-    private const string Author = "{your name here}";
+    private const string Author = "FixItFelix";
     private const string ModGuid = Author + "." + ModName;
     private const string ConfigFileName = ModGuid + ".cfg";
     private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
 
     private readonly Harmony _harmony = new(ModGuid);
 
-    public static readonly ManualLogSource ModTemplateLogger =
+    public static readonly ManualLogSource ListAllPrefabsLogger =
         BepInEx.Logging.Logger.CreateLogSource(ModName);
 
     private void Awake()
